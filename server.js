1150c73bc938d1571f15fb3308227ceb3ab72a5e
surendrababu134 <surendra134@gmail.com>
2017-07-04 10:53:12 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ for(var i=0; i < 12; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
 }
 
-var ciphertext = CryptoJS.AES.encrypt('surendra123', text);
+var ciphertext = CryptoJS.AES.encrypt('PasswordEncryption123$', text);
 
 var bytes = CryptoJS.AES.decrypt('U2FsdGVkX19eZ1qXyNf9U3SztP4tOvMIZ5qBWe9ArHA=', 'zYg8cysvGhNJ');
 var plaintext = bytes.toString(CryptoJS.enc.Utf8);
 
-console.log('Password in DataBase:'+ciphertext);
-console.log('Salt:'+text);
+console.log('Encrypted Password:'+ciphertext);
+console.log('Password encrypted code:'+text);
 console.log('Password you entered:'+plaintext);
